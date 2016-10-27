a740c89523d0ef4a8bb7398b4248ca98578e9bad
mattroz <matveyozanov@gmail.com>
2016-10-27 09:02:33 +0300

    pton and ntop have benn tested

--- sock_func_test.c
+++ sock_func_test.c
@@ -13,8 +13,9 @@ int main(int argc, char *argv[])
 	struct sockaddr_in sa;
 	struct sockaddr_in6 sa6;
 	char ipv4[INET_ADDRSTRLEN];
+	char src_ipv4[INET_ADDRSTRLEN] = "100.100.0.200";
 
-	inet_pton(AF_INET, "192.168.0.100", &(sa.sin_addr));
+	inet_pton(AF_INET, src_ipv4, &(sa.sin_addr));
 	printf("pton: %u\n", sa.sin_addr.s_addr);
 
 	inet_ntop(AF_INET, &(sa.sin_addr), ipv4, INET_ADDRSTRLEN);
